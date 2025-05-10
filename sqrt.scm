0fdfe3a6ec1fe5ce484d369fe8a37d495bc36ff6
George Spanos <gspanos@moby-it.com>
2025-05-10 18:16:57 +0300

    fix sqrt

--- sqrt.scm
+++ sqrt.scm
@@ -2,36 +2,40 @@
 ;; Newton's method works by iterative approximation using the formula:
 ;; next = (guess + x/guess) / 2 where x is the number we want to find sqrt of
 
+(define (good-enough? guess x)
+  (< (abs (- (* guess guess) x)) 0.001))
+
+(define (good-enough? guess x)
+  (< (abs (- (* guess guess) x)) 0.001))
+
+(define (improve guess x)
+  (/ (+ guess (/ x guess)) 2))
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+    guess
+    (sqrt-iter (improve guess x) x)))
+
 (define (sqrt x)
-  (define (good-enough? guess)
-    (< (abs (- (* guess guess) x)) 0.001))
-  
-  (define (improve guess)
-    (/ (+ guess (/ x guess)) 2))
-  
-  (define (sqrt-iter guess)
-    (if (good-enough? guess)
-        guess
-        (sqrt-iter (improve guess))))
-  
-  (sqrt-iter 1.0))
+  (sqrt-iter 1.0 x))
 
-  ;; Read a number from input and calculate its square root
-  (define (main)
-    (display "Enter a number: ")
-    (let ((input (read)))
-      (if (number? input)
-          (begin
-            (display "Square root of ")
-            (display input)
-            (display " is ")
-            (display (round (inexact (sqrt input))))
-            (newline))
-          (display "Please enter a valid number\n"))))
+;; Read a number from input and calculate its square root
+(define (main)
+  (display "Enter a number: ")
+  (let ((input (read)))
+    (if (number? input)
+      (begin
+        (display "Square root of ")
+        (display input)
+        (display " is ")
+        (display (round (inexact (sqrt input))))
+        (newline))
+      (display "Please enter a valid number\n"))))
 
-  ;; Call main function
-  (main)
+;; Call main function
+(main)
 
 ;; Example usage:
 ;; (sqrt 9)    ; should be close to 3
 ;; (sqrt 2)    ; should be close to 1.414
+
